684016ef3f56d747a10847399cbdfaad2230a215
dengzezhong <2207008527@qq.com>
2020-01-15 15:34:55 +0800

    v01
    v01

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import './assets/js/rem'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
-Vue.config.productionTip = false
+Vue.use(Vant)
+
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/view/Fair/Fair.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        友邻市集展示区
+    </div>
+</template>
